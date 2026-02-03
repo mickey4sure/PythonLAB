789d9255366c3e467cc8c80418ff61d35530f769
MiCKEY4SURE <mickeyforsure@gmail.com>
2026-02-03 23:31:05 +0530

    This file is a tutorial for using command prompt to sum two numbers in python

---
+++ add.py
@@ -0,0 +1,13 @@
+import sys
+num1 = float(sys.argv[1])
+num2 = float(sys.argv[2])
+sum = num1 + num2 
+print("Sum =", sum)
+
+'''Microsoft Windows [Version 10.0.26200.7623]
+(c) Microsoft Corporation. All rights reserved.
+
+D:\Python>python add.py 20 30
+Sum = 50.0
+
+D:\Python>''' 
